c43aff7e121789203da7d1e5993fb9d3efb4a50c
Michelle M <michellemoon236@gmail.com>
2019-03-20 00:09:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index(1) |name, index|
+  puts "#{index} #{name}"  
 end
 
 def summon_captain_planet# code an argument here
