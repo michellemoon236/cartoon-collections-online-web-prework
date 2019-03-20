a8ec0be18ce5ab1d8d0c332e8cd9bc8b6091eef4
Michelle M <michellemoon236@gmail.com>
2019-03-20 00:23:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese (list)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
+
+  list.find do |cheese|
+      cheese_types = ["cheddar", "gouda", "camembert"]
+      cheese.
 end
